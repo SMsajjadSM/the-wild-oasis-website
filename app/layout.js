6d96b59a8ed1669d9a49bbb40814265b583195e9
SMsajjadSM <sajjadsiadati1390@gmail.com>
2025-03-27 14:47:49 +0330

    new file:   app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,32 @@
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
+import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
+import { Josefin_Sans } from "next/font/google";
+export const metadata = {
+  // title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: " Welcome / The Wild Oasis ",
+  },
+  description:
+    "Luxurious cabin Hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountain and dark forest",
+};
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className} relative bg-primary-950 text-gray-50 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl w-full mx-auto">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
