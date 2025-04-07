d185aee7389555a00a90581c6de0ad1ae6b41799
SMsajjadSM <sajjadsiadati1390@gmail.com>
2025-04-07 18:02:47 +0330

    modified:   app/_lib/actions.js

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,7 +1,35 @@
 "use server";
 
-import { redirect } from "next/dist/server/api-utils";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
+}
+export async function signOutAction() {
+  await signOut({ redirectTo: "/" });
+}
+export async function updateGuest(formData) {
+  const session = await auth();
+  if (!session.user) throw new Error("You must be Login");
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  const nationalIDRegex = /^[a-zA-Z0-9]{6,12}$/;
+  if (!nationalIDRegex.test(nationalID)) {
+    throw new Error(
+      "National ID must be an alphanumeric string between 6 and 12 characters."
+    );
+  }
+  const updateData = { nationality, countryFlag, nationalID };
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user.guestId);
+
+  if (error) {
+    throw new Error("Guest could not be updated");
+  }
+  revalidatePath("/account/profile");
 }
